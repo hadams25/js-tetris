d91770f2943091956a3d8865a2f206614b8840f9
hadams25 <46791347+hadams25@users.noreply.github.com>
2022-07-24 22:23:55 -0500

    added basic functions to board class

--- javascript/board.js
+++ javascript/board.js
@@ -5,6 +5,21 @@ class board
         this.rows = rows;
         this.columns = columns;
         this.block_size = block_size;
+
+        //creates an array that can be accessed using [column][row] / [x][y]
         this.grid = new Array(this.columns).fill(new Array(this.rows).fill(0));
+    }
+
+    reset()
+    {
+        this.grid = new Array(this.columns).fill(new Array(this.rows).fill(0));
+    }
+
+    clear_row(row)
+    {
+        for(let i = 0; i < this.columns; i++)
+        {
+            this.grid[i][row] = 0;
+        }
     }
 }
